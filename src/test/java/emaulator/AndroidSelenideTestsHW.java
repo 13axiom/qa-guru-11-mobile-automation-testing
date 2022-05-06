7a6dfd1547591f9bcbe57195bfb208b0fd7d9c42
dmitriy.tokarev <dmitriy.tokarev@quadcode.com>
2022-05-06 18:23:25 +0300

    emulator commit

---
+++ src/test/java/emaulator/AndroidSelenideTestsHW.java
@@ -0,0 +1,31 @@
+package emaulator;
+
+import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.qameta.allure.Allure.step;
+
+public class AndroidSelenideTestsHW extends TestBase {
+
+    @Test
+    @Tag("selenide")
+    void searchTest() {
+        step("Skip introduction", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click();
+        });
+        step("Type search", () -> {
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("Appium");
+        });
+        step("Verify content found", () ->
+                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .shouldHave(sizeGreaterThan(0)));
+    }
+
+
+}
---
+++ src/test/java/emaulator/TestBase.java
@@ -0,0 +1,40 @@
+package emaulator;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.Attach.getSessionId;
+import static io.qameta.allure.Allure.step;
+
+public class TestBase {
+
+    @BeforeAll
+    public static void setup() {
+        addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.browser = drivers.EmulatorMobileDriver.class.getName();
+//        Configuration.startMaximized = false;
+        Configuration.browserSize = null;
+    }
+
+    @BeforeEach
+    public void startDriver() {
+        open();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+
+        step("Close driver", Selenide::closeWebDriver);
+    }
+
+}
