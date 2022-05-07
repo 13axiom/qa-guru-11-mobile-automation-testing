ef8d1b5ffeed027a5f6ba73086c2a0629c67bdb9
dmitriy.tokarev <dmitriy.tokarev@quadcode.com>
2022-05-07 17:15:16 +0300

    intermediate commit

--- src/test/java/emaulator/AndroidSelenideTestsHW.java
+++ src/test/java/emaulator/AndroidSelenideTestsHW.java
@@ -5,10 +5,13 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
+import static java.lang.Thread.sleep;
 
 public class AndroidSelenideTestsHW extends TestBase {
 
@@ -27,5 +30,24 @@ public class AndroidSelenideTestsHW extends TestBase {
                         .shouldHave(sizeGreaterThan(0)));
     }
 
+    @Test
+    @Tag("emulator_hw")
+    void hw1() throws InterruptedException {
+
+        step("1st page of introduction: checking header", () -> {
+                    $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                            .shouldHave(text("The Free Encyclopedia …in over 300 languages"));
+                });
+
+        step("1st page of introduction: checking that english has already been available in app", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/option_label")).shouldHave(text("1. English"));
+        });
+
+        step("1st page of introduction: checking that page has button for adding new languages", () -> {
+            $$(AppiumBy.id("android.widget.TextView")).findBy(text("ADD OR EDIT LANGUAGES")).click();
+           // $(AppiumBy.accessibilityId("ADD OR EDIT LANGUAGES")).shouldHave(attribute("enabled", "true"));
+
+        });
+    }
 
 }
